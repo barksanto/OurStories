931478d0fde22cd76ef220dccec4264839644bf7
Barkley <barkley.santo@gmail.com>
2022-08-10 23:01:32 +0800

    import materialize CDN and font awesome

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const router = express.Router()
 // @ login/ landing page
 // @ route = GET request to '/'
 router.get("/", (req, res) => {
-  res.send("Login!")
+  res.render("login")
 })
 
 // @ get request to "/dashboard"
 router.get("/dashboard", (req, res) => {
-  res.send("Dashboard!")
+  res.render("dashboard")
 })
 
 module.exports = router
